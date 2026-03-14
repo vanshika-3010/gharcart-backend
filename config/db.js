0e2a7314251bced436a06cc78cd41fd3df742a4f
Vanshika Mudgal <vanshikamudgal010@gmail.com>
2026-03-14 21:17:04 +0530

    update mongo connection

--- config/db.js
+++ config/db.js
@@ -1,18 +1,11 @@
-// backend/config/db.js
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const { MONGO_USER, MONGO_PASS, MONGO_CLUSTER, MONGO_DB } = process.env;
-
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority`;
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error('DB CONNECTION ERROR:', err);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("DB CONNECTION ERROR:", error);
     process.exit(1);
   }
 };
